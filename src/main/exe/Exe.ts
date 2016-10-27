aad6e50fe5c1436976e7e0de753210756aed198c
Klaus Reimer <k@ailis.de>
2016-10-27 21:21:34 +0200

    Finish reading of Exe information

--- src/main/exe/Exe.ts
+++ src/main/exe/Exe.ts
@@ -1,97 +1,199 @@
 /*
  * Copyright (C) 2016 Klaus Reimer <k@ailis.de>
  * See LICENSE.md for licensing information.
- *
- * This file provides a function used to unpack the WL.EXE file. It is based on
- * [unexepack.c](https://sourceforge.net/p/openkb/code/ci/master/tree/src/tools/unexecomp.c) written by
- * Vitaly Driedfruit.
  */
 
 import { unpackExe } from "./exepack";
-import { decodeHuffman } from "../io/huffman";
-import { decodeStringGroups } from "../io/string";
-import { BinaryReader } from "../io/BinaryReader";
+import { StringGroups, decodeStringGroups } from "../io/string";
 
+/** The offset of segment 2 in the unpacked WL.EXE file. */
+const SEG2 = 0xd020;
+
+/**
+ * Provides information from the WL.EXE file.
+ */
 export class Exe {
-    private readonly data: Uint8Array;
+    /** The intro strings. */
+    private introStrings: StringGroups;
 
+    /** Various unsorted strings. */
+    private messageStrings: StringGroups;
+
+    /** Various strings mostly used in inventory, skills and attribute screens. */
+    private inventoryStrings: StringGroups;
+
+    /** Character creation strings. */
+    private characterCreationStrings: StringGroups;
+
+    /** Promition strings. */
+    private promotionStrings: StringGroups;
+
+    /** Library strings. */
+    private libraryStrings: StringGroups;
+
+    /** Shop strings. */
+    private shopStrings: StringGroups;
+
+    /** Infirmary strings. */
+    private infirmaryStrings: StringGroups;
+
+    /** The map sizes of the maps in the two GAME files. */
+    private mapSizes: Uint8Array;
+
+    /** The map offsets of the maps in the two GAME files. */
+    private mapOffsets: Uint32Array;
+
+    /**
+     * Creates a new Exe information object from the given packed EXE content.
+     *
+     * @param data  The packed content of the WL.EXE file.
+     */
     private constructor(data: Uint8Array) {
-        this.data = data;
+        const unpacked = unpackExe(data);
+
+        // Decode global strings
+        this.introStrings = decodeStringGroups(unpacked, SEG2 + 0xa703, 527);
+        this.messageStrings = decodeStringGroups(unpacked, SEG2 + 0xab3e, 1661);
+        this.inventoryStrings = decodeStringGroups(unpacked, SEG2 + 0xb270, 1845);
+        this.characterCreationStrings = decodeStringGroups(unpacked, SEG2 + 0xce4B, 210);
+        this.promotionStrings = decodeStringGroups(unpacked, SEG2 + 0xd622, 1136);
+        this.libraryStrings = decodeStringGroups(unpacked, SEG2 + 0xdacc, 277);
+        this.shopStrings = decodeStringGroups(unpacked, SEG2 + 0xdbf8, 229);
+        this.infirmaryStrings = decodeStringGroups(unpacked, SEG2 + 0xdced, 369);
+
+        // Read the map sizes of the maps in the two GAME files.
+        this.mapSizes = new Uint8Array(unpacked.buffer, SEG2 + 0xbf1c, 42);
+
+        // Read the map offsets of the maps in the two GAME files.
+        this.mapOffsets = new Uint32Array(unpacked.slice(SEG2 + 0xbc7a, SEG2 + 0xbc7a + 42 * 4).buffer);
     }
 
+    /**
+     * Parses a WL.exe file and returns the information from it.
+     *
+     * @param data  The array containing the packed content of the WL.EXE file.
+     * @return The parsed information from the exe file.
+     */
     public static fromArray(data: Uint8Array): Exe {
-        return new Exe(unpackExe(data));
+        return new Exe(data);
     }
 
-    public getString(offset: number): string {
-        let i = offset;
-        let value: number;
-        const data = this.data;
-        let result = "";
-        while (i < data.length && (value = data[i++]) !== 0) {
-            result += String.fromCharCode(value);
-        }
-        return result;
-    }
-
-    public getIntroStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x17723, 527);
-    }
-
-    public findBestSize(offset: number, skip: number): number {
-        const ref = decodeStringGroups(this.data, offset, 10000);
-        while (skip--) {
-            let group = ref[ref.length - 1];
-            group.pop();
-            if (!group.length) {
-                ref.pop();
-            }
-        }
-        const refJSON = JSON.stringify(ref);
-        for (let i = 0; i < 10000; i++) {
+    /**
+     * Reads information from the given WL.EXE blob and returns it.
+     *
+     * @param blob  The WL.EXE blob.
+     * @return The exe information.
+     */
+    public static fromBlob(blob: Blob): Promise<Exe> {
+        return new Promise((resolve, reject) => {
             try {
-                const test = decodeStringGroups(this.data, offset, i);
-                const testJSON = JSON.stringify(test);
-                if (testJSON === refJSON) {
-                    return i;
-                }
+                const reader = new FileReader();
+                reader.onload = event => {
+                    resolve(Exe.fromArray(new Uint8Array(reader.result)));
+                };
+                reader.onerror = event => {
+                    reject(new Error("Unable to read WL.EXE: " + reader.error));
+                };
+                reader.readAsArrayBuffer(blob);
             } catch (e) {
+                reject(e);
             }
-        }
-        throw new Error("max size too large?");
+        });
     }
 
-    public getMessageStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x17b5e, 1661);
+    /**
+     * Returns the intro strings. They are used in the title of the game and contain the copyight message and
+     * the small introduction befor ethe game starts.
+     *
+     * @return The intro strings.
+     */
+    public getIntroStrings(): StringGroups {
+        return this.introStrings;
     }
 
-    public getInventoryStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x18290, 1847);
+    /**
+     * Returns various unsorted strings like some encounter messages, loot messages, weapon reloading and more.
+     *
+     * @return Various unsorted strings.
+     */
+    public getMessageStrings(): StringGroups {
+        return this.messageStrings;
     }
 
-    public getCreateCharacterStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x19e6b, 210);
+    /**
+     * Returns various strings which mostly have something to do with the players inventory but also contains
+     * strings for skills and attributes.
+     *
+     * @return Various strings mostly used in inventory, skills and attribute screens.
+     */
+    public getInventoryStrings(): StringGroups {
+        return this.inventoryStrings;
     }
 
-    public getPromotionStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x1a642, 1136);
+    /**
+     * Returns the strings used during character creation.
+     *
+     * @return Character creation strings.
+     */
+    public getCharacterCreationStrings(): StringGroups {
+        return this.characterCreationStrings;
     }
 
-    public getLibraryStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x1aaec, 277);
+    /**
+     * Returns strings used during promitions.
+     *
+     * @return Promition strings.
+     */
+    public getPromotionStrings(): StringGroups {
+        return this.promotionStrings;
     }
 
-    public getShopStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x1ac18, 229);
+    /**
+     * Returns strings used in libraries.
+     *
+     * @return Library strings.
+     */
+    public getLibraryStrings(): StringGroups {
+        return this.libraryStrings;
     }
 
-    public getInfirmaryStrings(): Array<Array<string>> {
-        return decodeStringGroups(this.data, 0x1ad0d, 369);
+    /**
+     * Returns strings used in shops.
+     *
+     * @return Shop strings.
+     */
+    public getShopStrings(): StringGroups {
+        return this.shopStrings;
+    }
+
+    /**
+     * Returns strings used in infirmaries.
+     *
+     * @return Infirmary strings.
+     */
+    public getInfirmaryStrings(): StringGroups {
+        return this.infirmaryStrings;
+    }
+
+    /**
+     * Returns the map size of the given map.
+     *
+     * @param disk  The disk index (0 or 1).
+     * @param map   The map index (0-19 for disk 0 and 0-21 for disk 1).
+     * @return The map size.
+     */
+    public getMapSize(disk: number, map: number) {
+        return this.mapSizes[disk * 20 + map];
+    }
+
+    /**
+     * Returns the map offset of the given map.
+     *
+     * @param disk  The disk index (0 or 1).
+     * @param map   The map index (0-19 for disk 0 and 0-21 for disk 1).
+     * @return The map offset.
+     */
+    public getMapOffset(disk: number, map: number) {
+        return this.mapOffsets[disk * 20 + map];
     }
 }
-
-import * as fs from "fs";
-const file = fs.readFileSync("/home/k/.steam/steam/SteamApps/common/Wasteland/rom/WL.EXE");
-const exe = Exe.fromArray(file);
-console.log(exe.getInfirmaryStrings());
-//console.log(exe.findBestSize(0x1ad0d, 3));
--- src/main/io/string.ts
+++ src/main/io/string.ts
@@ -1,9 +1,32 @@
+/*
+ * Copyright (C) 2016 Klaus Reimer <k@ailis.de>
+ * See LICENSE.md for licensing information.
+ */
+
 import { BinaryReader } from "./BinaryReader";
 
+/** A string group with up to four strings. */
+export type StringGroup = string[];
+
+/** A set of string groups. */
+export type StringGroups = StringGroup[];
+
+/**
+ * Read the string dictionary from the given reader. This is always a string of 60 characters.
+ *
+ * @param reader  The reader to read the string dictionary from.
+ * @return The read dictionary as a string with 60 characters.
+ */
 function readDictionary(reader: BinaryReader): string {
     return reader.readString(60);
 }
 
+/**
+ * Read string group pointers from the given reader. Invalid pointers are filtered out as good as possible.
+ *
+ * @param reader  The reader to read the string group pointers from.
+ * @return The found string group pointers.
+ */
 function readPointers(reader: BinaryReader): number[] {
     const pointers: number[] = [];
     let pointer = reader.readUint16();
@@ -20,10 +43,17 @@ function readPointers(reader: BinaryReader): number[] {
     return pointers;
 }
 
-function decodeStringGroup(dictionary: string, reader: BinaryReader): string[] {
+/**
+ * Decodes a string group read from the given reader.
+ *
+ * @param dictionary  The dictionary to use to decode the strings.
+ * @param reader      The reader to read the strings from.
+ * @return Array with up to four strings. May contain less than four if end of stream is reached.
+ */
+function decodeStringGroup(dictionary: string, reader: BinaryReader): StringGroup {
     let upper = false;
     let high = false;
-    let stringGroup: string[] = [];
+    let stringGroup: StringGroup = [];
     let string = "";
     while (reader.hasData(5) && stringGroup.length < 4) {
         const index = reader.readBits(5, true);
@@ -51,7 +81,15 @@ function decodeStringGroup(dictionary: string, reader: BinaryReader): string[] {
     return stringGroup;
 }
 
-export function decodeStringGroups(data: Uint8Array, offset: number, size: number): Array<Array<string>> {
+/**
+ * Decodes string groups from the given data.
+ *
+ * @param data  The data array to read the string groups from.
+ * @param offset  The starting offset where to find the string groups to read.
+ * @param size    The size of the block to decode.
+ * @return The decoded string groups.
+ */
+export function decodeStringGroups(data: Uint8Array, offset: number, size: number): StringGroups {
     const reader = new BinaryReader(data, offset, size);
     const dictionary = readDictionary(reader);
     const pointers = readPointers(reader);
