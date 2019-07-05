55a2ee29070d1a098027f85507ece918ef60b3d8
Klaus Reimer <k@ailis.de>
2019-07-05 08:28:36 +0200

    Fix demo

--- src/demo/system-config.js
+++
@@ -1,9 +0,0 @@
-System.config({
-    baseURL: "../../node_modules",
-    packages: {
-        "/": {
-            defaultExtension: "js"
-        }
-    },
-    packageConfigPaths: [ "@*/*/package.json", "*/package.json" ]
-});
