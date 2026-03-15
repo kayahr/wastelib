2bf5733fcab1a6ef6e2317b16800203d39adf9a2
Klaus Reimer <k@ailis.de>
2026-03-15 20:56:28 +0100

    More unit tests

--- src/test/ending/Ending.test.ts
+++ src/test/ending/Ending.test.ts
@@ -4,21 +4,44 @@
  */
 
 import { describe, it } from "node:test";
-import { assertEquals } from "@kayahr/assert";
+import { assertEquals, assertSame, assertThrowWithMessage } from "@kayahr/assert";
 import { readFile } from "node:fs/promises";
 import { assertMatchImage } from "../support/image.ts";
 import { createCanvasContext2D } from "../support/canvas.ts";
 import { Ending } from "../../main/ending/Ending.ts";
+import { openAsBlob } from "node:fs";
 
 describe("Ending", () => {
+    it("is iterable", async () => {
+        const ending = Ending.fromArray(await readFile("src/test/data/end.cpa"));
+        let index = 0;
+        for (const update of ending) {
+            assertSame(update, ending.getUpdate(index++));
+        }
+    });
     describe("fromArray", () => {
         it("reads END.CPA from array", async () => {
-            const array = await readFile("src/test/data/end.cpa");
-            console.log(array.length);
-            const ending = Ending.fromArray(array);
+            const ending = Ending.fromArray(await readFile("src/test/data/end.cpa"));
             assertEquals(ending.getWidth(), 288);
             assertEquals(ending.getHeight(), 128);
+            assertEquals(ending.getSize(), 15);
             await assertMatchImage(ending.toImageData(createCanvasContext2D()), "ending/000");
+        });
+    });
+    describe("fromBlob", () => {
+        it("reads END.CPA from blob", async () => {
+            const ending = await Ending.fromBlob(await openAsBlob("src/test/data/end.cpa"));
+            assertEquals(ending.getWidth(), 288);
+            assertEquals(ending.getHeight(), 128);
+            assertEquals(ending.getSize(), 15);
+            await assertMatchImage(ending.toImageData(createCanvasContext2D()), "ending/000");
+        });
+    });
+    describe("getUpdate", () => {
+        it("throws an error for invalid indices", async () => {
+            const ending = Ending.fromArray(await readFile("src/test/data/end.cpa"));
+            assertThrowWithMessage(() => ending.getUpdate(-1), RangeError, "Index out of bounds: -1");
+            assertThrowWithMessage(() => ending.getUpdate(15), RangeError, "Index out of bounds: 15");
         });
     });
 });
