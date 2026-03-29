dea1db6950a98cc0de1454a1d9a7e18309c2d9c1
Klaus Reimer <k@ailis.de>
2026-03-29 18:51:18 +0200

    Remove unused method

--- src/main/portrait/PortraitUpdate.ts
+++ src/main/portrait/PortraitUpdate.ts
@@ -30,20 +30,6 @@ export class PortraitUpdate implements Iterable<PortraitPatch> {
     }
 
     /**
-     * Returns the patch with the given index.
-     *
-     * @param index - The index of the patch to return.
-     * @returns The patch.
-     * @throws {@link !RangeError} if the index is out of bounds.
-     */
-    public getPatch(index: number): PortraitPatch {
-        if (index < 0 || index >= this.#patches.length) {
-           throw new RangeError(`Index out of bounds: ${index}`);
-        }
-        return this.#patches[index];
-    }
-
-    /**
      * Reads an animation update from the given reader. If the end of the animation has been reached then `null` is
      * returned.
      *
